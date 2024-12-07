387b5c678f3f32c01756413722ffb51cbfa7c3a2
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-12-07 19:41:00 +0530

    Implement CropHealthService with CRUD operations and data fetching; enhance API calls with withCredentials support

--- src/app/components/crop-health/crop-health.component.ts
+++ src/app/components/crop-health/crop-health.component.ts
@@ -1,9 +1,73 @@
 import { Component } from '@angular/core';
+import {
+  CropHealth,
+  CropHealthService,
+} from '../../services/crop-health.service';
+import { Farm, FarmsService } from '../../services/farms.service';
 
 @Component({
   selector: 'app-crop-health',
-  templateUrl: './crop-health.component.html'
+  templateUrl: './crop-health.component.html',
 })
 export class CropHealthComponent {
+  farms: Farm[] = [];
+  cropHealths: CropHealth[] = [];
+  error: string | null = null;
+  loading: boolean = true;
+
+  constructor(
+    private cropHealthService: CropHealthService,
+    private farmsService: FarmsService
+  ) {}
+
+  ngOnInit() {
+    //first we need to fetch all the farms for the current user using FarmService
+    //then we need to fetch all the crop healths for each farm using CropHealthService
+    //this is because we need to display crop healths for all the farms
+    //this is a nested request
+
+    this.farmsService.getFarms().subscribe(
+      (farms) => {
+        this.farms = farms;
+        for (const farm of this.farms) {
+          if (farm._id === '') {
+            continue;
+          }
+          this.cropHealthService.getCropHealthByFarm(farm._id!).subscribe(
+            (cropHealths) => {
+              this.cropHealths = this.cropHealths.concat(cropHealths);
+              this.loading = false;
+            },
+            (error) => {
+              console.error(error);
+              this.error = 'Failed to load crop healths';
+              this.loading = false;
+            }
+          );
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.error = 'Failed to load cropHealths';
+        this.loading = false;
+      }
+    );
+  }
+
+  deleteCropHealth(id: string) {
+    const ok = confirm('Are you sure you want to delete this farm?');
+    if (ok) {
+      this.cropHealthService.deleteCropHealth(id).subscribe(
+        () => {
+          this.cropHealths = this.cropHealths.filter(
+            (cropHealths) => cropHealths._id !== id
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
 
 }
--- src/app/services/crop-health.service.ts
+++ src/app/services/crop-health.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export interface CropHealth {
+  _id?: string;
+  farmId: {
+    _id?: string;
+    location: string;
+  }
+  healthStatus: string;
+  issueDetected?: string;
+  recommendation?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CropHealthService {
+  private apiUrl = 'http://localhost:5000/api/cropHealth/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getCropHealthByFarm(farmId: string) {
+    return this.httpClient.get<CropHealth[]>(`${this.apiUrl}farm/${farmId}`, { withCredentials: true });
+  }
+
+  getCropHealth(id: string) {
+    return this.httpClient.get<CropHealth>(`${this.apiUrl}${id}`,  { withCredentials: true });
+  }
+
+  createCropHealth(cropHealth: CropHealth) {
+    return this.httpClient.post<{}>(this.apiUrl, cropHealth, { withCredentials: true });
+  }
+
+  updateCropHealth(id: string, cropHealth: CropHealth) {
+    return this.httpClient.put<{ message: String, cropHealth: CropHealth }>(`${this.apiUrl}${id}`, cropHealth, { withCredentials: true });
+  }
+
+  deleteCropHealth(id: string) {
+    return this.httpClient.delete<{}>(`${this.apiUrl}${id}`, { withCredentials: true });
+  }
 }
