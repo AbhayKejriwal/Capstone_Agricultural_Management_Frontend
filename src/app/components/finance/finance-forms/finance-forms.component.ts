b18fea7c4d276a5854334052c83088c395aeecf1
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-12-08 13:16:53 +0530

    Implement finance forms with CRUD operations; enhance UI and integrate farm selection

--- src/app/components/finance/finance-forms/finance-forms.component.ts
+++ src/app/components/finance/finance-forms/finance-forms.component.ts
@@ -1,10 +1,106 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Farm, FarmsService } from '../../../services/farms.service';
+import { Finance, FinanceService } from '../../../services/finance.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-finance-forms',
-  templateUrl: './finance-forms.component.html',
-  styleUrl: './finance-forms.component.css'
+  templateUrl: './finance-forms.component.html'
 })
 export class FinanceFormsComponent {
+  farms: Farm[] = [];
+  financeForm: FormGroup;
+  editFinanceId!: string;
 
+  constructor(
+    private fb: FormBuilder,
+    private financeService: FinanceService,
+    private farmService: FarmsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.financeForm = this.fb.group({
+      farm: ['', Validators.required],
+      transactionType: ['', Validators.required],
+      amount: ['', Validators.required],
+      date: ['', Validators.required],
+      details: [''],
+    });
+  }
+
+  ngOnInit() {
+    this.editFinanceId = this.route.snapshot.params['id'];
+    if (this.editFinanceId) {
+      this.financeService.getFinance(this.editFinanceId).subscribe((result) => {
+        //patchvalue will populate the value in the form
+        this.financeForm.patchValue({
+          farm: result.farm, 
+          
+        });
+      });
+    }
+    this.farmService.getActiveFarms().subscribe((result) => {
+      this.farms = result;
+    });
+    console.log('id:', this.editFinanceId);
+  }
+
+  mapFormToFinance() {
+    return {
+      farmId: this.financeForm.value.farm._id as string,
+      farm: this.financeForm.value.farm.location as string,
+      transactionType: this.financeForm.value.transactionType as string,
+      amount: this.financeForm.value.amount as number,
+      date: this.financeForm.value.date as Date,
+      details: this.financeForm.value.details as string || undefined,
+    }
+  }
+
+  addFinance() {
+    if (this.financeForm.valid) {
+      // map form value to Finance interface
+      const finance: Finance = this.mapFormToFinance();
+
+      this.financeService.createFinance(finance).subscribe(
+        () => {
+          console.log('Finance created successfully');
+          this.navigateToDashboard();
+        },
+        (error) => {
+          console.log('Error:', error);
+          alert(`Error in creating Finance: ${error.error.message}`);
+        }
+      );
+    }
+    else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  updateFinance() {
+    if (this.financeForm.valid) {
+      // map form value to Finance interface
+      const finance: Finance = this.mapFormToFinance();
+
+      this.financeService.updateFinance(this.editFinanceId, finance).subscribe(
+        () => {
+          console.log('Finance updated successfully');
+          this.navigateToDashboard();
+        },
+        (error) => {
+          console.log('Error:', error);
+          alert(`Error in updating Finance: ${error.error.message}`);
+        }
+      );
+    } else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard'], {
+      state: { path: 'finance' }
+    }); 
+  }
 }
--- src/app/components/finance/finance.component.ts
+++ src/app/components/finance/finance.component.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
+import { Finance, FinanceService } from '../../services/finance.service';
+import { Farm, FarmsService } from '../../services/farms.service';
 
 @Component({
   selector: 'app-finance',
   templateUrl: './finance.component.html',
-  styleUrl: './finance.component.css'
 })
 export class FinanceComponent {
+  farms: Farm[] = [];
+  finances: Finance[] = [];
+  error: string | null = null;
+  loading: boolean = true;
+  nofarms: boolean = false;
+  totalIncome: number = 0;
+  totalExpense: number = 0;
+  totalCredit: number = 0;
 
+  constructor(
+    private financeService: FinanceService,
+    private farmsService: FarmsService
+  ) {}
+
+  ngOnInit() {
+    //first we need to fetch all the farms for the current user using FarmService
+    //then we need to fetch all the crop healths for each farm using FinanceService
+    //this is because we need to display crop healths for all the farms
+    //this is a nested request
+
+    this.farmsService.getActiveFarms().subscribe(
+      (farms) => {
+        this.farms = farms;
+        if (this.farms.length === 0) {
+          this.nofarms = true;
+          this.error = 'No farms found. Please add a farm first';
+          this.loading = false;
+        }
+        for (const farm of this.farms) {
+          // if (farm._id === '') {
+          //   continue;
+          // }
+          this.financeService.getFinanceByFarm(farm._id!).subscribe(
+            (finances) => {
+              this.finances = this.finances.concat(finances);
+              this.loading = false;
+            },
+            (error) => {
+              console.error(error);
+              this.error = 'Failed to load crop healths';
+              this.loading = false;
+            }
+          );
+          this.financeService.getFarmFinance(farm._id!).subscribe(
+            (data) => {
+              this.totalIncome += data.totalIncome;
+              this.totalExpense += data.totalExpense;
+              this.totalCredit += data.totalCredit;
+            },
+            (error) => {
+              console.error(error);
+            }
+          );
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.error = 'Failed to load finances';
+        this.loading = false;
+      }
+    );
+  }
+
+  deleteFinance(id: string) {
+    const ok = confirm('Are you sure you want to delete this farm?');
+    if (ok) {
+      this.financeService.deleteFinance(id).subscribe(
+        () => {
+          this.finances = this.finances.filter(
+            (finances) => finances._id !== id
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
 }
