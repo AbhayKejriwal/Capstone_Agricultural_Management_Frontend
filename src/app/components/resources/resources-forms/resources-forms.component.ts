1daa08094d68c70a29018893976e9e9cc8e5b1ea
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-12-08 13:27:12 +0530

    Implement resource management forms with CRUD operations; enhance UI and integrate farm selection

--- src/app/components/resources/resources-forms/resources-forms.component.ts
+++ src/app/components/resources/resources-forms/resources-forms.component.ts
@@ -1,10 +1,101 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ResourcesService, Resource } from '../../../services/resources.service';
+import { FarmsService, Farm } from '../../../services/farms.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-resources-forms',
-  templateUrl: './resources-forms.component.html',
-  styleUrl: './resources-forms.component.css'
+  templateUrl: './resources-forms.component.html'
 })
 export class ResourcesFormsComponent {
+  resourceForm: FormGroup;
+  farms: Farm[] = [];
+  editId!: string;
 
+  constructor(
+    private fb: FormBuilder,
+    private resourcesService: ResourcesService,
+    private farmService: FarmsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.resourceForm = this.fb.group({
+      farm: ['', Validators.required],
+      itemType: ['', Validators.required],
+      quantity: ['', Validators.required],
+      usageDate: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.editId = this.route.snapshot.params['id'];
+    if (this.editId) {
+      this.resourcesService.getResource(this.editId).subscribe((result) => {
+        this.resourceForm.patchValue({
+          farm: result.farm,
+          itemType: result.itemType,
+          quantity: result.quantity,
+          usageDate: result.usageDate
+        });
+      });
+    }
+    this.farmService.getActiveFarms().subscribe((result) => {
+      this.farms = result;
+    });
+  }
+
+  mapFormToResource() {
+    return {
+      farmId: this.resourceForm.value.farm._id as string,
+      farm: this.resourceForm.value.farm.location as string,
+      itemType: this.resourceForm.value.itemType as string,
+      quantity: this.resourceForm.value.quantity as number,
+      usageDate: this.resourceForm.value.usageDate as Date || undefined,
+    }
+  }
+
+  addResource() {
+    if (this.resourceForm.valid) {
+      const resource: Resource = this.mapFormToResource();
+
+      this.resourcesService.createResource(resource).subscribe(
+        () => {
+          console.log('Resource created successfully');
+          this.navigateToDashboard();
+        },
+        (error) => {
+          console.log('Error:', error);
+          alert(`Error in creating Resource: ${error.error.message}`);
+        }
+      );
+    } else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  updateResource() {
+    if (this.resourceForm.valid) {
+      const resource: Resource = this.mapFormToResource();
+
+      this.resourcesService.updateResource(this.editId, resource).subscribe(
+        () => {
+          console.log('Resource updated successfully');
+          this.navigateToDashboard();
+        },
+        (error) => {
+          console.log('Error:', error);
+          alert(`Error in updating Resource: ${error.error.message}`);
+        }
+      );
+    } else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard'], {
+      state: { path: 'resources' }
+    });
+  }
 }
--- src/app/components/resources/resources.component.ts
+++ src/app/components/resources/resources.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { Farm, FarmsService } from '../../services/farms.service';
+import { Resource, ResourcesService } from '../../services/resources.service';
 
 @Component({
   selector: 'app-resources',
-  templateUrl: './resources.component.html',
-  styleUrl: './resources.component.css'
+  templateUrl: './resources.component.html'
 })
 export class ResourcesComponent {
+  farms: Farm[] = [];
+  resources: Resource[] = [];
+  error: string | null = null;
+  loading: boolean = true;
+  nofarms: boolean = false;
 
+  constructor(
+    private resourceService: ResourcesService,
+    private farmsService: FarmsService
+  ) {}
+
+  ngOnInit() {
+    this.farmsService.getActiveFarms().subscribe(
+      (farms) => {
+        this.farms = farms;
+        if (this.farms.length === 0) {
+          this.nofarms = true;
+          this.error = 'No farms found. Please add a farm first';
+          this.loading = false;
+        }
+        for (const farm of this.farms) {
+          this.resourceService.getResourcesByFarm(farm._id!).subscribe(
+            (resources) => {
+              this.resources = this.resources.concat(resources);
+              this.loading = false;
+            },
+            (error) => {
+              console.error(error);
+              this.error = 'Failed to load resources';
+              this.loading = false;
+            }
+          );
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.error = 'Failed to load resources';
+        this.loading = false;
+      }
+    );
+  }
+
+  deleteResource(id: string) {
+    const ok = confirm('Are you sure you want to delete this farm?');
+    if (ok) {
+      this.resourceService.deleteResource(id).subscribe(
+        () => {
+          this.resources = this.resources.filter(
+            (resources) => resources._id !== id
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
 }
