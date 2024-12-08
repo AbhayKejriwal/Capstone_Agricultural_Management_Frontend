a9d6054ced20cc621c92e56011036c346106194e
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-12-08 13:17:04 +0530

    Implement inventory forms with CRUD operations; enhance UI and integrate farm selection

--- src/app/components/inventory/inventory-forms/inventory-forms.component.ts
+++ src/app/components/inventory/inventory-forms/inventory-forms.component.ts
@@ -1,10 +1,104 @@
 import { Component } from '@angular/core';
+import { Farm, FarmsService } from '../../../services/farms.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Inventory, InventoryService } from '../../../services/inventory.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-inventory-forms',
-  templateUrl: './inventory-forms.component.html',
-  styleUrl: './inventory-forms.component.css'
+  templateUrl: './inventory-forms.component.html'
 })
 export class InventoryFormsComponent {
+  farms: Farm[] = [];
+  inventoryForm: FormGroup;
+  editId!: string;
 
+  constructor(
+    private fb: FormBuilder,
+    private inventoryService: InventoryService,
+    private farmService: FarmsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.inventoryForm = this.fb.group({
+      farm: ['', Validators.required],
+      itemType: ['', Validators.required],
+      quantity: ['', Validators.required],
+      lastUpdated: ['']      
+    });
+  }
+
+  ngOnInit() {
+    this.editId = this.route.snapshot.params['id'];
+    if (this.editId) {
+      this.inventoryService.getInventory(this.editId).subscribe((result) => {
+        //patchvalue will populate the value in the form
+        this.inventoryForm.patchValue({
+          farm: result.farm, 
+          
+        });
+      });
+    }
+    this.farmService.getActiveFarms().subscribe((result) => {
+      this.farms = result;
+    });
+    console.log('id:', this.editId);
+  }
+
+  mapFormToInventory() {
+    return {
+      farmId: this.inventoryForm.value.farm._id as string,
+      farm: this.inventoryForm.value.farm.location as string,
+      itemType: this.inventoryForm.value.itemType as string,
+      quantity: this.inventoryForm.value.quantity as number,
+      lastUpdated: this.inventoryForm.value.lastUpdated as Date || undefined,
+    }
+  }
+
+  addInventory() {
+    if (this.inventoryForm.valid) {
+      // map form value to Inventory interface
+      const inventory: Inventory = this.mapFormToInventory();
+
+      this.inventoryService.createInventory(inventory).subscribe(
+        () => {
+          console.log('Inventory created successfully');
+          this.navigateToDashboard();
+        },
+        (error) => {
+          console.log('Error:', error);
+          alert(`Error in creating Inventory: ${error.error.message}`);
+        }
+      );
+    }
+    else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  updateInventory() {
+    if (this.inventoryForm.valid) {
+      // map form value to Inventory interface
+      const inventory: Inventory = this.mapFormToInventory();
+
+      this.inventoryService.updateInventory(this.editId, inventory).subscribe(
+        () => {
+          console.log('Inventory updated successfully');
+          this.navigateToDashboard();
+        },
+        (error) => {
+          console.log('Error:', error);
+          alert(`Error in updating Inventory: ${error.error.message}`);
+        }
+      );
+    } else {
+      alert('Please fill all the fields');
+    }
+  }
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard'], {
+      state: { path: 'inventory' }
+    }); 
+  }
 }
--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,10 +1,75 @@
 import { Component } from '@angular/core';
+import { Farm, FarmsService } from '../../services/farms.service';
+import { Inventory, InventoryService } from '../../services/inventory.service';
 
 @Component({
   selector: 'app-inventory',
-  templateUrl: './inventory.component.html',
-  styleUrl: './inventory.component.css'
+  templateUrl: './inventory.component.html'
 })
 export class InventoryComponent {
+  farms: Farm[] = [];
+  inventory: Inventory[] = [];
+  error: string | null = null;
+  loading: boolean = true;
+  nofarms: boolean = false;
 
+  constructor(
+    private inventoryService: InventoryService,
+    private farmsService: FarmsService
+  ) {}
+
+  ngOnInit() {
+    //first we need to fetch all the farms for the current user using FarmService
+    //then we need to fetch all the crop healths for each farm using inventoryService
+    //this is because we need to display crop healths for all the farms
+    //this is a nested request
+
+    this.farmsService.getActiveFarms().subscribe(
+      (farms) => {
+        this.farms = farms;
+        if (this.farms.length === 0) {
+          this.nofarms = true;
+          this.error = 'No farms found. Please add a farm first';
+          this.loading = false;
+        }
+        for (const farm of this.farms) {
+          // if (farm._id === '') {
+          //   continue;
+          // }
+          this.inventoryService.getInventoryByFarm(farm._id!).subscribe(
+            (inventory) => {
+              this.inventory = this.inventory.concat(inventory);
+              this.loading = false;
+            },
+            (error) => {
+              console.error(error);
+              this.error = 'Failed to load crop healths';
+              this.loading = false;
+            }
+          );
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.error = 'Failed to load inventory';
+        this.loading = false;
+      }
+    );
+  }
+
+  deleteInventory(id: string) {
+    const ok = confirm('Are you sure you want to delete this farm?');
+    if (ok) {
+      this.inventoryService.deleteInventory(id).subscribe(
+        () => {
+          this.inventory = this.inventory.filter(
+            (inventory) => inventory._id !== id
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
 }
